9f4e59d5ce7afc6b48f7756ac9b31e94d900e360
Robert Konrad <git2016@robdangero.us>
2016-10-17 00:09:44 +0200

    Fix build_debug.bat

--- build_debug.bat
+++ build_debug.bat
@@ -2,7 +2,7 @@ set PATH=..\depot_tools;%PATH%
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 call gn gen out.gn/debug --args="v8_enable_inspector=true is_debug=true is_component_build=true target_cpu=\"x64\""
 ninja -C out.gn/debug
-cd out.gn\release\obj
+cd out.gn\debug\obj
 cd v8_libbase
 lib /out:v8_libbase.lib *.obj
 cd ..
