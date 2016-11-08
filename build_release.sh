3dd58487585956c8f9a94f85f7b9ada20979d89b
Robert Konrad <git2016@robdangero.us>
2016-11-09 00:52:12 +0100

    Fix PATH in bash scripts

--- build_release.sh
+++ build_release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export PATH=`pwd`/depot_tools:"$PATH"
+export PATH=`pwd`/../depot_tools:"$PATH"
 gn gen out.gn/release --args="v8_enable_inspector=true is_debug=false is_official_build=true is_component_build=true"
 ninja -C out.gn/release
